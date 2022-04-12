29f630cdb2519d28a8336102ac74958d2ea75097
Emmanuel Kyei Nketia <eknketia001@st.ug.edu.gh>
2022-04-12 22:10:58 +0000

    Initial Commit

---
+++ BeMyEyeUI/BeMyEyeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeMyEyeUIApp.swift
+//  BeMyEyeUI
+//
+//  Created by Emmanuel K. Nketia on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BeMyEyeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
